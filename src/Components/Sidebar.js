7b6851d7ba177b54a4b1c733f8cebee252be7c91
Anupam kumar <anupamit011@gmail.com>
2022-01-27 16:36:15 +0530

    update the responsive sidebars

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -8,8 +8,15 @@ function Sidebar() {
         <ul className='SidebarList'>
             {SidebarData.map((val, key)=> {
                 return (
-                <li key={key} className='row' onClick={()=> {window.location.pathname = val.link}}>
-                    <div>{val.icon}</div> <div>{val.title}</div>
+                <li
+                    key={key} 
+                    className='row'
+                    id={window.location.pathname == val.link ? "active" : ""}
+                    onClick={()=> {
+                        window.location.pathname = val.link
+                        }}
+                >
+                    <div id='icon'>{val.icon}</div> <div id='title'>{val.title}</div>
                 </li>
                 );
             })}
